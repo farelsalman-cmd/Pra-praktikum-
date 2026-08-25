e95239bdeaf7d17cc72caff94d869bdb2c9a3ccb
farelsalman-cmd <farelsalman@students.untidar.ac.id>
2026-08-25 16:40:34 +0700

    Add files via upload

---
+++ Identitas.py
@@ -0,0 +1,14 @@
+#Menampilkan data diri
+print("=======================================================\n\t\t\t\tDATA DIRI MAHASISWA\n=======================================================")
+#Menampilkan Nama
+print("\t\nNama\t\t\t\t\t: M. Farel. Salman")
+#Menampilkan NIM
+print("\t\nNIM\t\t\t\t\t\t: 2605060003")
+#Menampilkan Tempat, Tanggal Lahir
+print("\t\nTempat, Tanggal Lahir\t: Magelang, 6 Juni 2008")
+#Menampilkan Jenis Kelamin
+print("\t\nJenis Kelaminm\t\t\t: Laki-laki")
+#Menampilkan Hobi
+print("\t\nHobi\t\t\t\t\t: Menggambar")
+#Penutup
+print("\n=======================================================")
